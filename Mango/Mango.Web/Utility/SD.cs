7d4be69759083ae5050b7b38b26320ca66a00bb5
sowndv02 <106914515+sowndv02@users.noreply.github.com>
2024-04-24 16:58:39 +0700

    [sondv] feat: Auth in Web project

--- Mango/Mango.Web/Utility/SD.cs
+++ Mango/Mango.Web/Utility/SD.cs
@@ -2,12 +2,15 @@
 {
     public class SD
     {
-        public static string CouponAPIBase {  get; set; }
+        public static string CouponAPIBase { get; set; }
+        public static string AuthAPIBase { get; set; }
+        public const string RoleAdmin = "ADMIN";
+        public const string RoleCustomer = "CUSTOMER";
         public enum ApiType
         {
-            GET, 
-            POST, 
-            PUT, 
+            GET,
+            POST,
+            PUT,
             DELETE
         }
     }
