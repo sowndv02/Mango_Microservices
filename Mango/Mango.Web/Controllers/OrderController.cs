eed556fd9e1e7a032bd7a4fc6d1bef7b682b860c
sowndv02 <daoson03112002@gmail.com>
2024-05-03 01:42:46 +0700

    [sondv] feat: Create orderdetail UI and add filter status

--- Mango/Mango.Web/Controllers/OrderController.cs
+++ Mango/Mango.Web/Controllers/OrderController.cs
@@ -3,6 +3,7 @@ using Mango.Web.Service.IService;
 using Mango.Web.Utility;
 using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
+using System.Collections.Generic;
 using System.IdentityModel.Tokens.Jwt;
 
 namespace Mango.Web.Controllers
@@ -20,19 +21,87 @@ namespace Mango.Web.Controllers
             return View();
         }
 
-        [HttpGet]
-        public IActionResult GetAll()
+
+        public async Task<IActionResult> OrderDetail(int orderId)
         {
-            IEnumerable<OrderHeaderDto> list = new();
+            OrderHeaderDto orderHeaderDto = new OrderHeaderDto();
+            string userId = User.Claims.Where(x => x.Type == JwtRegisteredClaimNames.Sub)?.FirstOrDefault()?.Value;
+
+            var response = await _orderService.GetOrder(orderId);
+            if (response != null && response.IsSuccess)
+            {
+                orderHeaderDto = JsonConvert.DeserializeObject<OrderHeaderDto>(Convert.ToString(response.Result));
+            }
+            if (!User.IsInRole(SD.RoleAdmin) && userId != orderHeaderDto.UserId)
+            {
+                return NotFound();
+            }
+            return View(orderHeaderDto);
+        }
+
+        [HttpPost("OrderReadyForPickup")]
+        public async Task<IActionResult> OrderReadyForPickup(int orderId)
+        {
+            var response = await _orderService.UpdateOrderStatus(orderId, SD.Status_ReadyForPickup);
+            if (response != null && response.IsSuccess)
+            {
+                TempData["success"] = "Status updated successfully";
+                return RedirectToAction(nameof(OrderDetail), new { orderId = orderId });
+            }
+            return View();
+        }
+
+        [HttpPost("CompleteOrder")]
+        public async Task<IActionResult> CompleteOrder(int orderId)
+        {
+            var response = await _orderService.UpdateOrderStatus(orderId, SD.Status_Completed);
+            if (response != null && response.IsSuccess)
+            {
+                TempData["success"] = "Status updated successfully";
+                return RedirectToAction(nameof(OrderDetail), new { orderId = orderId });
+            }
+            return View();
+        }
+
+        [HttpPost("CancelOrder")]
+        public async Task<IActionResult> CancelOrder(int orderId)
+        {
+            var response = await _orderService.UpdateOrderStatus(orderId, SD.Status_Cancelled);
+            if (response != null && response.IsSuccess)
+            {
+                TempData["success"] = "Status updated successfully";
+                return RedirectToAction(nameof(OrderDetail), new { orderId = orderId });
+            }
+            return View();
+        }
+
+
+        [HttpGet]
+        public IActionResult GetAll(string status)
+        {
+            IEnumerable<OrderHeaderDto> list;
             string userId = "";
             if (!User.IsInRole(SD.RoleAdmin))
             {
                 userId = User.Claims.Where(x => x.Type == JwtRegisteredClaimNames.Sub)?.FirstOrDefault()?.Value;
             }
             ResponseDto response = _orderService.GetAllOrder(userId).GetAwaiter().GetResult();
-            if(response !+ null && response.IsSuccess)
+            if(response != null && response.IsSuccess)
             {
                 list = JsonConvert.DeserializeObject<List<OrderHeaderDto>>(Convert.ToString(response.Result));
+                switch (status)
+                {
+                    case "approved":
+                        list = list.Where(x => x.Status == SD.Status_Approved);
+                        break;
+                    case "readyforpickup":
+                        list = list.Where(x => x.Status == SD.Status_ReadyForPickup);
+                        break;
+                    case "cancelled":
+                        list = list.Where(x => x.Status == SD.Status_Cancelled || x.Status == SD.Status_Refunded);
+                        break;
+                    default: break;
+                }
             }
             else
             {
