3b14ae33f077b62cdd79f4971818978281afaf41
sowndv02 <daoson03112002@gmail.com>
2024-05-04 00:28:56 +0700

    [sondv] feat: Upload Images Upload image when create or update product and delete img when delete product. Allowed extension and max file size

--- Mango/Mango.Web/Utility/SD.cs
+++ Mango/Mango.Web/Utility/SD.cs
@@ -24,5 +24,12 @@
         public const string Status_Completed = "Completed";
         public const string Status_Refunded = "Refunded";
         public const string Status_Cancelled = "Cancelled";
+
+        public enum ContentType
+        {
+            Json,
+            MultipartFormData
+        }
     }
+    
 }
